534112e0d98f7729942513781cfe45cbd56dd365
charit nanduri <charithnanduri03@gmail.com>
2021-10-26 22:12:07 +0000

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1);
+
+const eqArrays = function(arr1,arr2) {
+  let output = [];
+  if (arr1.length !== arr2.length) {
+    output.push(false);
+  } else {
+    for (let val in arr1) {
+      if (arr1[val] !== arr2[val]) {
+        output.push(false);
+      }
+    }
+  }
+  if (output.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
