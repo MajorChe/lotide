378c39815fe7bea927d2cfa463a85d6d29a19781
charit nanduri <charithnanduri03@gmail.com>
2021-10-27 22:16:49 +0000

    add countletter code to  letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual(1, 3);
+
+const countLetters = function(arr) {
+  let obj = {};
+  arr = arr.split('');
+  for (let val of arr) {
+    if (val !== ' ') {
+      obj[val] = arr.filter(function(letter) {
+        return letter === val;
+      }).length;
+    }
+  }
+  return obj;
+};
+
+console.log(countLetters("lighthouse in the house "));
+
 const letterPositions = function(sentence) {
   const results = {};
   // logic to update results here
