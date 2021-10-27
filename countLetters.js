9641c7097d03059190d340e340a30448fd50c0b8
charit nanduri <charithnanduri03@gmail.com>
2021-10-27 22:08:32 +0000

    add countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual(1, 3);
+
+const countLetters = function(arr) {
+  let obj = {};
+  arr = arr.split('');
+  for (let val of arr) {
+    if (val !== ' ') {
+      obj[val] = arr.filter(function(letter) {
+        return letter === val;
+      }).length;
+    }
+  }
+  return obj;
+};
+
+console.log(countLetters("lighthouse in the house "));
