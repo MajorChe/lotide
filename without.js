fffa8f60720007879e6adf4195b851d8e867f3e8
charit nanduri <charithnanduri03@gmail.com>
2021-10-27 00:00:51 +0000

    add without.js

---
+++ without.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1,arr2) {
+  let output = [];
+  if (arr1.length !== arr2.length) {
+    output.push(false);
+  } else {
+    for (let val in arr1) {
+      if (arr1[val] !== arr2[val]) {
+        output.push(false);
+      }
+    }
+  }
+  if (output.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actualArray,expectedArray) {
+  let check = eqArrays(actualArray,expectedArray);
+  if (check === true) {
+    console.log(`✅✅✅  Assertion Passed: ${actualArray} === ${expectedArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actualArray} !== ${expectedArray}`);
+  }
+};
+
+const without = function(source,itemsToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      newArray = source.filter(function(item) {
+        return !itemsToRemove.includes(item);
+      });
+    }
+  }
+  return newArray;
+};
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
