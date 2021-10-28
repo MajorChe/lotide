d066523f655e6d299b9bde95089bf996f6ad74b3
charit nanduri <charithnanduri03@gmail.com>
2021-10-28 23:13:17 +0000

    add test cases and asserArrayEqual to takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,30 @@
+const eqArrays = function(arr1,arr2) {
+  let output = [];
+  if (arr1.length !== arr2.length) {
+    output.push(false);
+  } else {
+    for (let val in arr1) {
+      if (arr1[val] !== arr2[val]) {
+        output.push(false);
+      }
+    }
+  }
+  if (output.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const assertArraysEqual = function (actualArray,expectedArray){
+  let check = eqArrays(actualArray,expectedArray);
+  if (check === true) {
+    console.log(`✅✅✅  Assertion Passed: ${actualArray} === ${expectedArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actualArray} !== ${expectedArray}`);
+  }
+};
+
 const takeUntil = function(array,callback){
   const result = [];
   let stopper = 0;
@@ -15,6 +42,7 @@ const takeUntil = function(array,callback){
 const array1 = [4,74,23,-1,0,10,2,4,5];
 const result1 = takeUntil(array1,x => x < 0);
 console.log(result1);
+console.log(assertArraysEqual(result1,[4,74,23]));
 
 console.log('---');
 
