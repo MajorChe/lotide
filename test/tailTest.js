52e89d00a9445c658d6eb73ebcc1cf7b8983e183
charit nanduri <charithnanduri03@gmail.com>
2021-11-01 21:26:13 +0000

    use index.js in test files

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,12 @@
-const tail = require('../tail');
+const _ = require('../index');
 const assert = require('chai').assert;
 
 describe('#tail', () => {
   it("returns [2,3] for [1,2,3]", () => {
-    assert.deepEqual(tail([1,2,3]),[2,3]);
+    assert.deepEqual(_.tail([1,2,3]),[2,3]);
   });
 
   it("returns [2,3,4] for [1,2,3,4]", () => {
-    assert.deepEqual(tail([1,2,3,4]),[2,3,4]);
+    assert.deepEqual(_.tail([1,2,3,4]),[2,3,4]);
   });
 });
