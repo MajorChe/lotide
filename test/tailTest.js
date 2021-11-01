959e1aff576e579b7d73c4728b2acb98788cbbe2
charit nanduri <charithnanduri03@gmail.com>
2021-11-01 18:56:08 +0000

    add another test case to tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -14,4 +14,8 @@ describe('#tail', () => {
   it("returns [2,3] for [1,2,3]", () => {
     assert.deepEqual(tail([1,2,3]),[2,3]);
   });
+
+  it("returns [2,3,4] for [1,2,3,4]", () => {
+    assert.deepEqual(tail([1,2,3,4]),[2,3,4]);
+  });
 });
