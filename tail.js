5f36fc882e63167fefd3d093767c10fddf9f33fe
charit nanduri <charithnanduri03@gmail.com>
2021-11-01 17:43:54 +0000

    removed test functions from tail.js

--- tail.js
+++ tail.js
@@ -1,20 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 3);
-
-const tail = function(array) {
+const tail = function(array) {        // Tail function is defined
   const newTail = array.slice(1);
   return newTail;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
