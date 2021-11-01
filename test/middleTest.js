d3ed57e18d0829957384924709054f1c503a4650
charit nanduri <charithnanduri03@gmail.com>
2021-11-01 19:15:28 +0000

    add chai assertions and mocha test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,25 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
-const assertEqual = require('../assertEqual');
-assertArraysEqual((middle([1,2,3])),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]); 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
-assertArraysEqual(middle([1,2]),[])
+
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("check to see if middle element of [1,2,3] is [2]", () => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+
+  it("check to see if middle element of [1,2,3,4,5] is [3]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]),[3]);
+  });
+
+  it("check to see if middle element of [1,2,3,4] is [2,3]", () => {
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  });
+
+  it("check to see if middle element of [1,2,3,4,5,6] is [3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]),[3,4]);
+  });
+
+  it("check to see if middle element of [1,2] is []", () => {
+    assert.deepEqual(middle([1,2]),[]);
+  });
+});
