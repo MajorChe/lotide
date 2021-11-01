4ad5c4ea418f5b7c32987191870eb0d6094ad21e
charit nanduri <charithnanduri03@gmail.com>
2021-11-01 18:18:36 +0000

    refactor middle.js using require and export

--- middle.js
+++ middle.js
@@ -1,29 +1,6 @@
-const eqArrays = function(arr1,arr2) {
-  let output = [];
-  if (arr1.length !== arr2.length) {
-    output.push(false);
-  } else {
-    for (let val in arr1) {
-      if (arr1[val] !== arr2[val]) {
-        output.push(false);
-      }
-    }
-  }
-  if (output.length !== 0) {
-    return false;
-  } else {
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function (actualArray,expectedArray){
-  let check = eqArrays(actualArray,expectedArray);
-  if (check === true) {
-    console.log(`✅✅✅  Assertion Passed: ${actualArray} === ${expectedArray}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actualArray} !== ${expectedArray}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(arr) {
   const middleIndex = Math.floor(arr.length / 2);
@@ -35,13 +12,9 @@ const middle = function(arr) {
   } else {
     return arr.slice(middleIndex,middleIndex + 1);
   }
-}
+};
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([1,2]));
+module.exports  = middle;
 
 
 
